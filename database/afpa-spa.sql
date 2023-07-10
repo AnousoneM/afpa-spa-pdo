367ece83973b033d0d938dfe53e0824461315957
AnousoneM <anousone.mounivongs@gmail.com>
2023-07-10 17:49:09 +0200

    mise à jour bdd

--- database/afpa-spa.sql
+++ database/afpa-spa.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 5.1.1
+-- version 5.2.0
 -- https://www.phpmyadmin.net/
 --
--- Hôte : localhost:3306
--- Généré le : dim. 09 juil. 2023 à 23:41
--- Version du serveur : 5.7.33
--- Version de PHP : 7.4.26
+-- Hôte : localhost:8889
+-- Généré le : lun. 10 juil. 2023 à 15:48
+-- Version du serveur : 5.7.39
+-- Version de PHP : 8.2.0
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -38,6 +38,7 @@ CREATE TABLE `animals` (
   `ani_microchipped` tinyint(1) NOT NULL,
   `ani_tattooed` tinyint(1) NOT NULL,
   `ani_vaccinated` tinyint(1) NOT NULL,
+  `ani_description` text NOT NULL,
   `col_id` int(11) NOT NULL,
   `spe_id` int(11) NOT NULL,
   `bre_id` int(11) NOT NULL
@@ -46,20 +47,20 @@ CREATE TABLE `animals` (
 -- --------------------------------------------------------
 
 --
--- Structure de la table `breed`
+-- Structure de la table `breeds`
 --
 
-CREATE TABLE `breed` (
+CREATE TABLE `breeds` (
   `bre_id` int(11) NOT NULL,
   `bre_name` varchar(30) NOT NULL,
   `spe_id` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
--- Déchargement des données de la table `breed`
+-- Déchargement des données de la table `breeds`
 --
 
-INSERT INTO `breed` (`bre_id`, `bre_name`, `spe_id`) VALUES
+INSERT INTO `breeds` (`bre_id`, `bre_name`, `spe_id`) VALUES
 (1, 'akita inu', 1),
 (2, 'husky', 1),
 (3, 'beagle', 1),
@@ -95,7 +96,8 @@ INSERT INTO `colors` (`col_id`, `col_name`) VALUES
 (4, 'beige'),
 (5, 'roux'),
 (6, 'gris'),
-(7, 'tigré');
+(7, 'tigré'),
+(8, 'sable');
 
 -- --------------------------------------------------------
 
@@ -130,9 +132,9 @@ ALTER TABLE `animals`
   ADD KEY `animals_breed1_FK` (`bre_id`);
 
 --
--- Index pour la table `breed`
+-- Index pour la table `breeds`
 --
-ALTER TABLE `breed`
+ALTER TABLE `breeds`
   ADD PRIMARY KEY (`bre_id`),
   ADD KEY `breed_species_FK` (`spe_id`);
 
@@ -159,16 +161,16 @@ ALTER TABLE `animals`
   MODIFY `ani_id` int(11) NOT NULL AUTO_INCREMENT;
 
 --
--- AUTO_INCREMENT pour la table `breed`
+-- AUTO_INCREMENT pour la table `breeds`
 --
-ALTER TABLE `breed`
+ALTER TABLE `breeds`
   MODIFY `bre_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=13;
 
 --
 -- AUTO_INCREMENT pour la table `colors`
 --
 ALTER TABLE `colors`
-  MODIFY `col_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
+  MODIFY `col_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
 
 --
 -- AUTO_INCREMENT pour la table `species`
@@ -184,14 +186,14 @@ ALTER TABLE `species`
 -- Contraintes pour la table `animals`
 --
 ALTER TABLE `animals`
-  ADD CONSTRAINT `animals_breed1_FK` FOREIGN KEY (`bre_id`) REFERENCES `breed` (`bre_id`),
+  ADD CONSTRAINT `animals_breed1_FK` FOREIGN KEY (`bre_id`) REFERENCES `breeds` (`bre_id`),
   ADD CONSTRAINT `animals_colors_FK` FOREIGN KEY (`col_id`) REFERENCES `colors` (`col_id`),
   ADD CONSTRAINT `animals_species0_FK` FOREIGN KEY (`spe_id`) REFERENCES `species` (`spe_id`);
 
 --
--- Contraintes pour la table `breed`
+-- Contraintes pour la table `breeds`
 --
-ALTER TABLE `breed`
+ALTER TABLE `breeds`
   ADD CONSTRAINT `breed_species_FK` FOREIGN KEY (`spe_id`) REFERENCES `species` (`spe_id`);
 COMMIT;
 
