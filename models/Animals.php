ee4f57b4889128d9422b024edc2663a92b404e81
anousonem <anousone.mounivongs@gmail.com>
2023-07-19 03:16:46 +0200

    update query ok

--- models/Animals.php
+++ models/Animals.php
@@ -88,7 +88,7 @@ class Animals
 
             return $result;
         } catch (PDOException $e) {
-            // test unitaire pour vérifier que la liste n'a pas été recupérée et connaitre la raison
+            // test unitaire pour connaitre la raison qui a empêché la récupération des animaux
             // echo 'Erreur : ' . $e->getMessage();
             return false;
         }
@@ -126,7 +126,7 @@ class Animals
             $stmt->rowCount() != 0 ? $result = $stmt->fetch(PDO::FETCH_ASSOC) : $result = false;
             return $result;
         } catch (PDOException $e) {
-            // test unitaire pour vérifier que l'animal n'a pas pu recupérée et connaitre la raison
+            // test unitaire pour connaitre la raison qui a empêché la récupération des détails de l'animal
             // echo 'Erreur : ' . $e->getMessage();
             return false;
         }
@@ -157,7 +157,50 @@ class Animals
             $stmt->execute() ? $result = true : $result = false;
             return $result;
         } catch (PDOException $e) {
-            // test unitaire pour vérifier que l'animal n'a pas pu recupérée et connaitre la raison
+            // test unitaire pour connaitre la raison qui a empêché la suppression de l'animal
+            // echo 'Erreur : ' . $e->getMessage();
+            return false;
+        }
+    }
+
+    /**
+     * Modifie un animal dans la base de données
+     * @param array $inputs Tableau contenant les données du formulaire
+     * @return bool Retourne true si l'animal a bien été modifié, false si KO
+     */
+    public function updateAnimal(array $inputs): bool
+    {
+        try {
+            // Creation d'une instance de connexion à la base de données
+            $pdo = Database::createInstancePDO();
+
+            // requête SQL pour ajouter un animal avec des marqueurs nominatifs
+            $sql = 'UPDATE `animals` SET `ani_name` = :name, `ani_sex` = :sex, `ani_birthdate` = :birthdate, `ani_arrivaldate` = :arrivaldate, `ani_microchipped` = :microchipped , `ani_tattooed` = :tattooed, `ani_vaccinated` = :vaccinated, `ani_description` = :description, `ani_picture` = :picture, `ani_weight` = :weight, `col_id` = :color, `spe_id` = :specie, `bre_id` = :breed WHERE `ani_id` = :id';
+
+            // On prépare la requête avant de l'exécuter
+            $stmt = $pdo->prepare($sql);
+
+            // On injecte les valeurs dans la requête et nous utilisons la méthode bindValue pour se prémunir des injections SQL
+            $stmt->bindValue(':id', Form::safeData($inputs['id']), PDO::PARAM_INT);
+            
+            $stmt->bindValue(':name', Form::safeData($inputs['name']), PDO::PARAM_STR);
+            $stmt->bindValue(':sex', Form::safeData($inputs['sex']), PDO::PARAM_STR);
+            $stmt->bindValue(':birthdate', Form::safeData($inputs['birthdate']), PDO::PARAM_STR);
+            $stmt->bindValue(':arrivaldate', Form::safeData($inputs['arrivaldate']), PDO::PARAM_STR);
+            $stmt->bindValue(':microchipped', isset($inputs['microchipped']) ? Form::safeData($inputs['microchipped']) : 0, PDO::PARAM_BOOL);
+            $stmt->bindValue(':tattooed', isset($inputs['tattooed']) ? Form::safeData($inputs['tattooed']) : 0, PDO::PARAM_BOOL);
+            $stmt->bindValue(':vaccinated', isset($inputs['vaccinated']) ? Form::safeData($inputs['vaccinated']) : 0, PDO::PARAM_BOOL);
+            $stmt->bindValue(':description', Form::safeData($inputs['description']), PDO::PARAM_STR);
+            $stmt->bindValue(':picture', $inputs['specie'] == 1 ? 'dog.webp' : 'cat.webp', PDO::PARAM_STR);
+            $stmt->bindValue(':weight', Form::safeData($inputs['weight']), PDO::PARAM_STR);
+            $stmt->bindValue(':color', Form::safeData($inputs['color']), PDO::PARAM_INT);
+            $stmt->bindValue(':specie', Form::safeData($inputs['specie']), PDO::PARAM_INT);
+            $stmt->bindValue(':breed', Form::safeData($inputs['breed']), PDO::PARAM_INT);
+
+            // On exécute la requête, elle sera true si elle a réussi, dans le cas contraire il y aura une exception
+            return $stmt->execute();
+        } catch (PDOException $e) {
+            // test unitaire pour connaitre la raison qui a empêché la modification de l'animal
             // echo 'Erreur : ' . $e->getMessage();
             return false;
         }
